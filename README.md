82beba27795073b55a6c71069ed6f2b236732d5b
wiccy46 <eric1986_chn@yahoo.com>
2019-08-13 21:33:23 +0200

    readme edit

---
+++ README.md
@@ -0,0 +1,5 @@
+# audio2numpy
+
+## Description
+
+audio2numpy load an audio file and directly ouputs the audio data as a numpy array and its sampling rate. 
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='audio2numpy',
+    version='0.1',
+    packages=['audio2numpy'],
+    license='MIT',
+    author='Jiajun Yang'
+    long_description=open('README.md').read(),
+    keywords=['audio, audio reader'],
+    url='https://github.com/wiccy46/audio2numpy'
+)
