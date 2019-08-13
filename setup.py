b8212bdb18f50fe50a78010e6403eca4ffa7bada
wiccy46 <eric1986_chn@yahoo.com>
2019-08-13 22:43:32 +0200

    add license, start writing setup and manifest.in

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from distutils.core import setup
+import os
+
+version = {}
+
+def _read(fn):
+    path = os.path.join(os.path.dirname(__file__), fn)
+    return open(path).read()
 
 setup(
     name='audio2numpy',
-    version='0.1',
+    version=version['__version__'],
     packages=['audio2numpy'],
     license='MIT',
     author='Jiajun Yang'
-    long_description=open('README.md').read(),
+    author_email='thejyang@gmail.com'
+    long_description=_read('README.md'),
     keywords=['audio, audio reader'],
-    url='https://github.com/wiccy46/audio2numpy'
+    url='https://github.com/wiccy46/audio2numpy',
+
 )
