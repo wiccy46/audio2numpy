849fec7da18f37533f5bea0b2e473698c568cd06
wiccy46 <eric1986_chn@yahoo.com>
2019-08-14 23:00:25 +0200

    now tested on 2.7, 3.5, 3.6, 3.7

--- setup.py
+++ setup.py
@@ -34,6 +34,9 @@ setup(
     classifiers=[
         'Topic :: Multimedia :: Sound/Audio',
         'Topic :: Multimedia :: Sound/Audio :: Conversion',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
 )
